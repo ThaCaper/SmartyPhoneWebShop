2a8c493576dd3ea5c04ba49ce061e20306ca9ca2
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-12-16 13:02:01 +0100

    Hot fix

--- src/app/pages/min-side/min-side.component.ts
+++ src/app/pages/min-side/min-side.component.ts
@@ -10,11 +10,41 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./min-side.component.css']
 })
 export class MinSideComponent implements OnInit {
-
-
-  constructor() { }
+  id: number;
+  user: User;
+  userForm = new FormGroup({
+    id: new FormControl({value: '', disabled: true}),
+    firstName: new FormControl({value: '', disabled: true}),
+    lastName: new FormControl({value: '', disabled: true}),
+    email: new FormControl({value: '', disabled: true}),
+    phoneNumber: new FormControl({value: '', disabled: true}),
+    street: new FormControl({value: '', disabled: true}),
+    zipCode: new FormControl({value: '', disabled: true}),
+    country: new FormControl(''),
+    username: new FormControl({value: '', disabled: true}),
+    password: new FormControl('')
+  });
+  constructor(private route: ActivatedRoute,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUserById(this.id)
+      .subscribe(userFromRest => {
+        this.user = userFromRest;
+        this.userForm.patchValue({
+          id: userFromRest.id,
+          firstName: userFromRest.firstName,
+          lastName: userFromRest.lastName,
+          email: userFromRest.email,
+          phoneNumber: userFromRest.phoneNumber,
+          country: userFromRest.country,
+          street: userFromRest.street,
+          zipCode: userFromRest.zipCode,
+          username: userFromRest.userName,
+          password: userFromRest.password
+        });
+      });
   }
 
 }
